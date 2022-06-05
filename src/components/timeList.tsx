e27930212e1c2e147743d97bf0948a3dcc70eac9
Gforcedev <tecshotclark@gmail.com>
2022-06-05 14:04:06 +0100

    Style timelist a bit and format tailwind config

--- src/components/timeList.tsx
+++ src/components/timeList.tsx
@@ -8,14 +8,21 @@ const TimeList = () => {
   const times = timesQuery.data;
 
   return (
-    times &&
-    times.map((t: Time) => {
-      return (
-        <div className="pt-2 bg-purple-600" key={t.id}>
-          {milliDisplay(t.time)}
-        </div>
-      );
-    })
+    <div className="flex justify-center pt-20">
+      <div className="w-1/3 text-center m-12 h-64 overflow-scroll">
+        {times &&
+          times.map((t: Time) => {
+            return (
+              <div
+                className="pt-3 pb-3 bg-purple-600 p-2 m-2 rounded"
+                key={t.id}
+              >
+                {milliDisplay(t.time)}
+              </div>
+            );
+          })}
+      </div>
+    </div>
   );
 };
 
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -24,6 +24,7 @@ export const appRouter = createRouter()
           orderBy: {
             createdAt: 'desc',
           },
+          take: 50,
         });
       }
 
